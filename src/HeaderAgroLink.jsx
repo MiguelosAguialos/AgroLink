fc505987f456fc59277a1b7288ac76a59e25ca55
MiguelosAguialos <aguiarmiguel65@gmail.com>
2025-03-26 23:24:52 -0300

    fix: home route header

--- src/HeaderAgroLink.jsx
+++ src/HeaderAgroLink.jsx
@@ -11,13 +11,19 @@ function HeaderAgroLink() {
           {/* Logo */}
           <div className="col-auto d-flex align-items-center brand-col">
             <Link to="/">
-              <img src={logoFundoVerde} alt="AgroLink Logo" className="logo-big" />
+              <img
+                src={logoFundoVerde}
+                alt="AgroLink Logo"
+                className="logo-big"
+              />
             </Link>
           </div>
 
           {/* Área do Usuário + Botão de Entrar */}
           <div className="col-auto d-flex align-items-center">
-            <Link to="/login" className="btn btn-outline-light me-3">Entrar</Link>
+            <Link to="/login" className="btn btn-outline-light me-3">
+              Entrar
+            </Link>
             <img src={userIcon} alt="Usuário" className="user-icon" />
           </div>
         </div>
@@ -25,13 +31,21 @@ function HeaderAgroLink() {
         {/* Menu inferior */}
         <div className="row mt-3 bottom-header">
           <div className="col d-flex justify-content-center gap-4 menu-bottom">
-            <Link to="/" className="menu-link">Home</Link>
-            <Link to="#" className="menu-link">Postos de Alimentos</Link>
-            <Link to="#" className="menu-link">Comunidade</Link>
-            <Link to="#" className="menu-link">Sobre Nós</Link>
-            <Link to="/suporte-e-feedback" className="menu-link">Suporte e Feedback</Link>
-
-
+            <Link to="/home" className="menu-link">
+              Home
+            </Link>
+            <Link to="#" className="menu-link">
+              Postos de Alimentos
+            </Link>
+            <Link to="#" className="menu-link">
+              Comunidade
+            </Link>
+            <Link to="#" className="menu-link">
+              Sobre Nós
+            </Link>
+            <Link to="/suporte-e-feedback" className="menu-link">
+              Suporte e Feedback
+            </Link>
           </div>
         </div>
       </div>
