7ce28a5af884195341777aa5239e3ad3393da158
MiguelosAguialos <aguiarmiguel65@gmail.com>
2025-03-26 21:39:30 -0300

    feat: merge login into dev

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,46 +1,114 @@
 import React from "react";
 import "./Login.css";
 import agrolinkLogo from "../../assets/logo-fundo-branco.svg";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
 
 export default function Login() {
+  const testUser = {
+    email: "teste@gmail.com",
+    password: "123",
+  };
+
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    if (data.email == testUser.email && data.password == testUser.password) {
+      Swal.fire({
+        title: "Sucesso!",
+        text: "Validação de credenciais está funcionando!",
+        icon: "success",
+        heightAuto: false,
+        timer: 2000,
+      });
+    } else {
+      Swal.fire({
+        title: "Erro",
+        text: "Credenciais inválidas!",
+        icon: "error",
+        confirmButtonText: "Voltar",
+        heightAuto: false,
+        timer: 2000,
+      });
+    }
+  };
   return (
-    <div className="d-flex flex-column justify-content-center align-items-center">
-      {/* Mobile */}
-      <section className="main--mobile d-lg-none container-fluid d-flex flex-column align-items-center">
-        <img src={agrolinkLogo} className="img-fluid" alt="logo" />
-        <h1 className="m-2">AGROLINK PORTAL</h1>
-        <form className="mt-4 d-flex flex-column">
-          <div className="form-group">
-            <label className="pb-1">Email</label>
-            <input type="email" className="form-control" placeholder="Digite seu email!" />
-            <small className="form-text text-muted">Nunca iremos compartilhar este email com ninguém!</small>
+    <div class="container-fluid d-flex flex-column justify-content-center align-items-center h-100">
+      {/* Mobile screen */}
+      <section class="main--mobile d-lg-none container-fluid d-flex flex-column align-items-center">
+        <img src={agrolinkLogo} class="img-fluid" alt="logo" />
+        <h1 class="m-2">AGROLINK PORTAL</h1>
+        <form class="mt-4 d-flex flex-column" onSubmit={handleSubmit(onSubmit)}>
+          <div class="form-group">
+            <label class="pb-1" for="email-field">
+              Email
+            </label>
+            <input
+              type="email"
+              {...register("email", { required: true })}
+              class="form-control"
+              aria-describedby="emailHelp"
+              placeholder="Digite seu email!"
+            />
+            <small id="emailHelp" class="form-text text-muted">
+              Nunca iremos compartilhar este email com ninguém!
+            </small>
           </div>
-          <div className="form-group mt-4">
-            <label className="pb-1">Senha</label>
-            <input type="password" className="form-control" placeholder="Digite sua senha!" />
+          <div class="form-group mt-4">
+            <label class="pb-1" for="password-field">
+              Senha
+            </label>
+            <input
+              type="password"
+              class="form-control"
+              {...register("password", { required: true })}
+              placeholder="Digite sua senha!"
+            />
           </div>
-          <button type="submit" className="btn btn-success mt-4 mb-4">Entrar</button>
+          <button type="submit" class="btn btn-success mt-4 mb-4">
+            Entrar
+          </button>
         </form>
       </section>
-
-      {/* Desktop */}
-      <section className="main--desktop d-none d-lg-flex flex-column">
-        <div className="main--content bg-light border rounded row p-0">
-          <div className="left-side col-4 d-flex flex-column align-items-center"></div>
-          <div className="right-side col d-flex flex-column align-items-center justify-content-center">
-            <img src={agrolinkLogo} className="img-fluid" alt="logo" />
-            <h1 className="m-2">AGROLINK PORTAL</h1>
-            <form className="mt-4 d-flex flex-column">
-              <div className="form-group">
-                <label className="pb-1">Email</label>
-                <input type="email" className="form-control" placeholder="Digite seu email!" />
-                <small className="form-text text-muted">Nunca iremos compartilhar este email com ninguém!</small>
+      {/* Desktop screen */}
+      <section class="main--desktop d-none d-lg-flex flex-column">
+        <div class="main--content bg-light border rounded row p-0">
+          <div class="left-side rounded col-4 d-flex flex-column align-items-center"></div>
+          <div class="right-side col d-flex flex-column align-items-center justify-content-center">
+            <img src={agrolinkLogo} class="img-fluid" alt="logo" />
+            <h1 class="m-2">AGROLINK PORTAL</h1>
+            <form
+              class="mt-4 d-flex flex-column"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <div class="form-group">
+                <label class="pb-1" for="email-field">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  class="form-control"
+                  {...register("email", { required: true })}
+                  aria-describedby="emailHelp"
+                  placeholder="Digite seu email!"
+                />
+                <small id="emailHelp" class="form-text text-muted">
+                  Nunca iremos compartilhar este email com ninguém!
+                </small>
               </div>
-              <div className="form-group mt-4">
-                <label className="pb-1">Senha</label>
-                <input type="password" className="form-control" placeholder="Digite sua senha!" />
+              <div class="form-group mt-4">
+                <label class="pb-1" for="password-field">
+                  Senha
+                </label>
+                <input
+                  type="password"
+                  class="form-control"
+                  {...register("password", { required: true })}
+                  placeholder="Digite sua senha!"
+                />
               </div>
-              <button type="submit" className="btn btn-success mt-4 mb-4">Entrar</button>
+              <button type="submit" class="btn btn-success mt-4 mb-4">
+                Entrar
+              </button>
             </form>
           </div>
         </div>
